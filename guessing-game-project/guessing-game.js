c906bd900071efcda4835a7efb72fa9555c70722
Erika <brandonerika96@gmail.com>
2024-05-29 20:24:05 -0400

    readline setup

--- guessing-game-project/guessing-game.js
+++ guessing-game-project/guessing-game.js
@@ -1,19 +1,26 @@
+//import the readline module into our file
+const readline = require('readline');
 
+//create an interface where we can talk w user
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
 let secretNumber = 10;
 
-function checkGuess(num){
+function checkGuess(num) {
     let number = Number(num);
 
-    if(number>secretNumber){
+    if (number>secretNumber) {
         console.log("Too high.");
         return false;
     }
-    if(number<secretNumber){
+    if (number<secretNumber) {
         console.log("Too low.");
         return false;
     }
-    if(number === secretNumber){
+    if (number === secretNumber) {
         console.log("Correct!");
         return true;
     }
