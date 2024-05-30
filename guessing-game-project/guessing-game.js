79a0dd10cec6616a8aab9379231a9122e625cb24
Erika <brandonerika96@gmail.com>
2024-05-29 20:43:45 -0400

    askGuess function completed

--- guessing-game-project/guessing-game.js
+++ guessing-game-project/guessing-game.js
@@ -12,11 +12,11 @@ let secretNumber = 10;
 function checkGuess(num) {
     let number = Number(num);
 
-    if (number>secretNumber) {
+    if (number > secretNumber) {
         console.log("Too high.");
         return false;
     }
-    if (number<secretNumber) {
+    if (number < secretNumber) {
         console.log("Too low.");
         return false;
     }
@@ -26,7 +26,22 @@ function checkGuess(num) {
     }
 }
 
+function askGuess() {
+    rl.question('Enter a guess: ', (answer) => {
+        let correct = checkGuess(answer); //invokes checkGuess function
+        if (!correct) {
+            askGuess();
+        } else {
+            console.log('You win!')
+            rl.close(); //closes interface
+        }
+      });
+}
+
 //Test calls for checkGuess
 // console.log(checkGuess('42'));
 // console.log(checkGuess('10'));
 // console.log(checkGuess('5'));
+
+//Testing askGuess/start the game (phase1)
+askGuess();
